f316eb1b82c9a798995aa685dcc8f513f5f9593e
DanielSrb <srb.dan@gmail.com>
2018-02-03 20:32:26 +0100

    4 space indentation

--- gulpfile.js
+++ gulpfile.js
@@ -19,69 +19,69 @@ var plumber = require('gulp-plumber');
 
 // runs django dev server
 gulp.task('runserver', gulp.series(function(done){
-  exec('python manage.py runserver --settings=pyconcz_2018.settings.local', function(err, stdout, stderr){
-    console.log(stdout);
-    console.log(stderr);
-  });
-  done();
+    exec('python manage.py runserver --settings=pyconcz_2018.settings.local', function(err, stdout, stderr){
+        console.log(stdout);
+        console.log(stderr);
+    });
+    done();
 }));
 
 
 // delete all previously generated CSS
 gulp.task('clean-css', gulp.series(function(){
-  return del(['./pyconcz_2018/static/css/'], { force: true });
+    return del(['./pyconcz_2018/static/css/'], { force: true });
 }));
 
 
 // delete all previously generated images
 gulp.task('clean-img', gulp.series(function(){
-  return del(['./pyconcz_2018/static/img/'], { force: true });
+    return del(['./pyconcz_2018/static/img/'], { force: true });
 }));
 
 
 // delete all previously generated JavaScript
 gulp.task('clean-js', gulp.series(function(){
-  return del(['./pyconcz_2018/static/js/'], { force: true });
+    return del(['./pyconcz_2018/static/js/'], { force: true });
 }));
 
 
 // copy images
 gulp.task('copy-img', gulp.series('clean-img', function(){
-  return gulp
-    .src(['./pyconcz_2018/static_src/img/**/*'])
-    .pipe(gulp.dest('./pyconcz_2018/static/img/'))
-    .pipe(browserSync.stream());
+    return gulp
+        .src(['./pyconcz_2018/static_src/img/**/*'])
+        .pipe(gulp.dest('./pyconcz_2018/static/img/'))
+        .pipe(browserSync.stream());
 }));
 
 
 // compile CSS
 gulp.task('compile-css', gulp.series('clean-css', function(){
-  return gulp
-    .src('./pyconcz_2018/static_src/scss/pyconcz.scss') // scss source
-    .pipe(plumber())
-    .pipe(sourcemaps.init()) // sourcemap for developer tools
-    .pipe(sass())
-    .pipe(postcss([
-      autoprefixer({
-        'browsers': [
-          '> 0.2%',
-          'last 3 versions'
-        ]
-      }),
-      flexbugs()
-    ])) // add vendor prefixes, fix flexbox bugs
-    .pipe(cleanCSS({
-      compatibility: 'ie11',
-      roundingPrecision: 4,
-      advanced: true,
-      rebase: false,
-      specialComments: 0,
-      processImport: false,
-      inline: ['none']
-    }))
-    .pipe(sourcemaps.write('.'))
-    .pipe(gulp.dest('./pyconcz_2018/static/css/')) // resulting CSS
-    .pipe(browserSync.stream()); // tell BrowserSync to inject CSS
+    return gulp
+        .src('./pyconcz_2018/static_src/scss/pyconcz.scss') // scss source
+        .pipe(plumber())
+        .pipe(sourcemaps.init()) // sourcemap for developer tools
+        .pipe(sass())
+        .pipe(postcss([
+            autoprefixer({
+                'browsers': [
+                    '> 0.2%',
+                    'last 3 versions'
+                ]
+            }),
+            flexbugs()
+        ])) // add vendor prefixes, fix flexbox bugs
+        .pipe(cleanCSS({
+            compatibility: 'ie11',
+            roundingPrecision: 4,
+            advanced: true,
+            rebase: false,
+            specialComments: 0,
+            processImport: false,
+            inline: ['none']
+        }))
+        .pipe(sourcemaps.write('.'))
+        .pipe(gulp.dest('./pyconcz_2018/static/css/')) // resulting CSS
+        .pipe(browserSync.stream()); // tell BrowserSync to inject CSS
 }));
 
 
@@ -91,20 +91,20 @@ gulp.task('build', gulp.parallel('compile-css', 'copy-img')); // build everythin
 // Build files and proxy dev server on http://localhost:3000
 // Watch for changes in files and do what is neccessary
 gulp.task(
-  'develop',
-  gulp.series('build', 'runserver', function(){
-    browserSync.init({
-      proxy: {
-        target: 'http://127.0.0.1:8000' // Django is running here
-      },
-      open: false,
-      files: [
-        './pyconcz_2018/templates/**/*.html'
-      ]
-    });
-    gulp.watch('./pyconcz_2018/static_src/scss/**/*.scss', gulp.series('compile-css')); // watcher for SCSS
-    gulp.watch('./pyconcz_2018/static_src/img/**/*', gulp.series('copy-img')); // watcher for images
-  })
+    'develop',
+    gulp.series('build', 'runserver', function(){
+        browserSync.init({
+            proxy: {
+                target: 'http://127.0.0.1:8000' // Django is running here
+            },
+            open: false,
+            files: [
+                './pyconcz_2018/templates/**/*.html'
+            ]
+        });
+        gulp.watch('./pyconcz_2018/static_src/scss/**/*.scss', gulp.series('compile-css')); // watcher for SCSS
+        gulp.watch('./pyconcz_2018/static_src/img/**/*', gulp.series('copy-img')); // watcher for images
+    })
 );
 
 
