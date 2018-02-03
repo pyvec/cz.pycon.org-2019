c6ce446e1bc6ad68fc03ac7f69a966a9ba7c4f6f
DanielSrb <srb.dan@gmail.com>
2018-02-04 00:49:32 +0100

    Bootstrap 4 - from now on new CSS will be only Bootstrap (removed compilation of CSS from old SCSS files) - first page (proposals/talks_about.html) is made completely with Bootstrap CSS. - menus are shared with old templates (keeping them backward compatible)

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,12 @@
 var gulp = require('gulp');
 
-var autoprefixer = require('autoprefixer');
-var flexbugs = require('postcss-flexbugs-fixes');
 var browserSync = require('browser-sync').create();
 var del = require('del');
-var postcss = require('gulp-postcss');
 var sass = require('gulp-sass');
 var sourcemaps = require('gulp-sourcemaps');
+var postcss = require('gulp-postcss');
+var autoprefixer = require('autoprefixer');
 var csso = require('gulp-csso');
-var watch = require('gulp-watch');
 var exec = require('child_process').exec;
 var plumber = require('gulp-plumber');
 
@@ -29,7 +27,7 @@ gulp.task('runserver', gulp.series(function(done){
 
 // delete all previously generated CSS
 gulp.task('clean-css', gulp.series(function(){
-    return del(['./pyconcz_2018/static/css/'], { force: true });
+    return del(['./pyconcz_2018/static/css/pyconcz18.*'], { force: true });
 }));
 
 
@@ -57,18 +55,35 @@ gulp.task('copy-img', gulp.series('clean-img', function(){
 // compile CSS
 gulp.task('compile-css', gulp.series('clean-css', function(){
     return gulp
-        .src('./pyconcz_2018/static_src/scss/pyconcz.scss') // scss source
+        .src('./pyconcz_2018/static_src/scss/pyconcz18/pyconcz18.scss') // scss source
         .pipe(plumber())
-        .pipe(sourcemaps.init()) // sourcemap for developer tools
-        .pipe(sass())
+        .pipe(sourcemaps.init()) // initalizes a sourcemap
+        .pipe(sass().on('error', sass.logError)) // compile SCSS to CSS
         .pipe(postcss([
-            autoprefixer({
-                'browsers': [
-                    '> 0.2%',
-                    'last 3 versions'
-                ]
-            }),
-            flexbugs()
+            autoprefixer( // automatically adds vendor prefixes if needed
+                // supported browsers (from Bootstrap 4 beta: see https://github.com/twbs/bootstrap/blob/v4-dev/package.json#L136-L147)
+                //
+                // Official browser support policy:
+                // https://getbootstrap.com/docs/4.0/getting-started/browsers-devices/#supported-browsers
+                //
+                "last 1 major version",
+                ">= 1%",
+                "Chrome >= 45", // Exact version number here is kinda arbitrary
+                "Firefox >= 38",
+                // Note: Edge versions in Autoprefixer & Can I Use refer to the EdgeHTML rendering engine version,
+                // NOT the Edge app version shown in Edge's "About" screen.
+                // For example, at the time of writing, Edge 20 on an up-to-date system uses EdgeHTML 12.
+                // See also https://github.com/Fyrd/caniuse/issues/1928
+                "Edge >= 12",
+                "Explorer >= 10",
+                // Out of leniency, we prefix these 1 version further back than the official policy.
+                "iOS >= 9",
+                "Safari >= 9",
+                // The following remain NOT officially supported, but we're lenient and include their prefixes to avoid severely breaking in them.
+                "Android >= 4.4",
+                "Opera >= 30"
+            ),
+            require('postcss-flexbugs-fixes') // fixes flex bugs if possible: see https://github.com/philipwalton/flexbugs
         ])) // add vendor prefixes, fix flexbox bugs
         .pipe(csso()) // compresses CSS
         .pipe(sourcemaps.write('.'))
