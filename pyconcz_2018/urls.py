5e12f2ae5dc4d7aba232f066bced4c81499b089e
starenka <starenka0@gmail.com>
2018-04-17 16:15:15 +0200

    temp programme url change

--- pyconcz_2018/urls.py
+++ pyconcz_2018/urls.py
@@ -12,14 +12,14 @@ prefixed_urlpatterns = [
     url(r'^proposals/', include('pyconcz_2018.proposals.urls')),
 
     url(r'^about/team/', include('pyconcz_2018.team.urls')),
-    #url(r'^programme/', include('pyconcz_2018.programme.urls')),
+    url(r'^programme/FIXME/', include('pyconcz_2018.programme.urls')),
     url(r'^sponsors/', include('pyconcz_2018.sponsors.urls')),
     #url(r'^intermission/', include('pyconcz_2018.intermission.urls', namespace='intermission')),
 
     # static pages
     url(r'^proposals/$',
-       TemplateView.as_view(template_name='proposals/talks_about.html'),
-       name='proposals'),
+        TemplateView.as_view(template_name='proposals/talks_about.html'),
+        name='proposals'),
     url(r'^tickets/$',
         TemplateView.as_view(template_name='pages/tickets.html'),
         name='tickets'),
