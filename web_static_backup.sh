aba284297ffd3f5e562a421011b29447a99bf1bd
Lumir Balhar <lbalhar@redhat.com>
2018-11-05 19:43:54 +0100

    Script for download a whole website to folder as backup of previous year.

---
+++ web_static_backup.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Make a static copy of pycon.cz website before a new year edition
+
+if [ -z $1 ]; then
+    echo "First argument has to be a year to backup (2018 for example)"
+    exit 1
+fi
+
+wget \
+    --recursive \
+    --page-requisites \
+    --html-extension \
+    --convert-links \
+    --domains cz.pycon.org \
+    --no-parent \
+        cz.pycon.org/$1
+
+mv cz.pycon.org/$1 $1
+mv cz.pycon.org/$1.html $1/index.html
