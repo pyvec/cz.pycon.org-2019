9d007e88331fe8e54a9675f13ad90ade591e8542
DanielSrb <srb.dan@gmail.com>
2019-03-12 18:39:26 +0100

    temp form for workshop cfp say CfP is closed on info page

--- pyconcz/proposals/urls.py
+++ pyconcz/proposals/urls.py
@@ -6,6 +6,7 @@ from pyconcz.proposals.views import (
 
 urlpatterns = [
     url('^(?P<key>[^/]*)/$', proposal_about, name='proposal_about'),
-    url('^(?P<key>[^/]*)/form/$', proposal_create, name='proposal_form'),
+    # url('^(?P<key>[^/]*)/form/$', proposal_create, name='proposal_form'),
+    url('^(?P<key>[^/]*)/form-temp/$', proposal_create, name='proposal_form'),
     url('^(?P<key>[^/]*)/sent/$', proposal_success, name='proposal_success'),
 ]
