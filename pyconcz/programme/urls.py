5b08a0311ff8af4ddbce16d50830bc7dba1e1057
DanielSrb <srb.dan@gmail.com>
2019-04-06 11:54:37 +0200

    preview of all speakers, talks and workshops

--- pyconcz/programme/urls.py
+++ pyconcz/programme/urls.py
@@ -1,11 +1,12 @@
 from django.conf.urls import url
 from django.views.generic import RedirectView
 
-from .views import speakers_list, schedule, talk_detail
+from .views import speakers_list, schedule, talk_detail, preview
 
 urlpatterns = [
     url('^$', RedirectView.as_view(pattern_name='speakers_list'), {'type': 'talks'}),
     url('^(?P<type>(talks|workshops))/$', speakers_list, name="speakers_list"),
     url('^detail/(?P<type>(talk|workshop))/(?P<talk_id>\d+)/$', talk_detail, name='talk_detail'),
     url('^schedule/$', schedule, name="programme_schedule"),
+    url('^preview/$', preview, name="preview"),
 ]
