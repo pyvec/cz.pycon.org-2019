3b9a6cd3d88ed09e11540d2e937aa9b90e7ddb97
DanielSrb <srb.dan@gmail.com>
2019-05-03 20:09:05 +0200

    url cleanup (flat is better than nested) - flattened urls and made permanent redirects from old urls - moved proposal url to urls there

--- pyconcz/urls.py
+++ pyconcz/urls.py
@@ -6,55 +6,39 @@ from django.views.generic import TemplateView, RedirectView
 
 from pyconcz.common.views import homepage
 
-'''
-prefixed_urlpatterns = [
-    url(r'^intermission/', include('pyconcz.intermission.urls', namespace='intermission')),
-    # static pages
-    url(r'^about/transparency-report/$',
-        TemplateView.as_view(template_name='pages/transparency.html'),
-        name='about_transparency'),
-]
-'''
-
 prefixed_urlpatterns = [
     url(r'^$', homepage, name='homepage'),
     url(r'^programme/', include('pyconcz.programme.urls')),
-    url(r'^about/team/', include('pyconcz.team.urls')),
+    url(r'^team/', include('pyconcz.team.urls')),
     url(r'^proposals/', include('pyconcz.proposals.urls')),
     url(r'^announcements/', include('pyconcz.announcements.urls')),
     url(r'^sponsors/', include('pyconcz.sponsors.urls')),
     url(r'^tickets/', include('pyconcz.common.urls')),
 
     # static pages
-    url(r'about/code/$',
-        TemplateView.as_view(template_name='pages/code.html'),
-        name='about_code'),
-    url(r'^about/privacy/$',
-        TemplateView.as_view(template_name='pages/privacy.html'),
-        name='about_privacy'),
-    url(r'^proposals/$',
-        TemplateView.as_view(template_name='proposals/talks_about.html'),
-        name='proposals'),
-    url(r'^pattern-lib/$',
-        TemplateView.as_view(template_name='pages/pattern-lib.html'),
-        name='pattern_lib'),
-    url(r'^ostrava/$',
-        TemplateView.as_view(template_name='pages/ostrava.html'),
-        name='city'),
-    url(r'^venue/$',
-        TemplateView.as_view(template_name='pages/venue.html'),
-        name='venue'),
-    url(r'^board-game-night/$',
-        TemplateView.as_view(template_name='pages/board_game_night.html'),
-        name='board_game_night'),
+    url(r'^coc/$', TemplateView.as_view(template_name='pages/code.html'), name='about_code'),
+    url(r'^privacy-policy/$', TemplateView.as_view(template_name='pages/privacy.html'), name='about_privacy'),
+    url(r'^ostrava/$', TemplateView.as_view(template_name='pages/ostrava.html'), name='city'),
+    url(r'^venue/$', TemplateView.as_view(template_name='pages/venue.html'), name='venue'),
+    url(r'^board-game-night/$', TemplateView.as_view(template_name='pages/board_game_night.html'), name='board_game_night'),
+    url(r'^pattern-lib/$', TemplateView.as_view(template_name='pages/pattern-lib.html'), name='pattern_lib'),
+
+    # url(r'^intermission/', include('pyconcz.intermission.urls', namespace='intermission')),
+    # static pages
+    # url(r'^transparency-report/$', TemplateView.as_view(template_name='pages/transparency.html'), name='about_transparency'),
+
+    # old urls redirect (delete when changing 2019 to 2020)
+    url(r'^about/code/?$', RedirectView.as_view(url='/2019/coc/', permanent=True)),
+    url(r'^about/privacy/?$', RedirectView.as_view(url='/2019/privacy-policy/', permanent=True)),
+    url(r'^about/team/?$', RedirectView.as_view(url='/2019/team/', permanent=True)),
 ]
 
 urlpatterns = (
-    static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
-    static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
-    [
-        url(r'^2019/', include(prefixed_urlpatterns)),
-        url(r'^admin/', include(admin.site.urls)),
-        url(r'^$', RedirectView.as_view(url='/2019/')),
-    ]
+        static(settings.STATIC_URL, document_root=settings.STATIC_ROOT) +
+        static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT) +
+        [
+            url(r'^2019/', include(prefixed_urlpatterns)),
+            url(r'^admin/', include(admin.site.urls)),
+            url(r'^$', RedirectView.as_view(url='/2019/')),
+        ]
 )
