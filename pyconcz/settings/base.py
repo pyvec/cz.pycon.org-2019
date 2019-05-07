1e6a13ba5d51e7561986901ef4d67874d4d172a9
Lumir Balhar <lbalhar@redhat.com>
2019-05-07 12:28:09 +0200

    Room names for talks and workshops
    Fixes: https://github.com/pyvec/cz.pycon.org-2019/issues/133

--- pyconcz/settings/base.py
+++ pyconcz/settings/base.py
@@ -181,19 +181,16 @@ SLACK_WEBHOOK = ''  # This variable is set in local.py
 TALKS_DATES = [datetime.date(2019, 6, 14), datetime.date(2019, 6, 15)]
 WORKSHOPS_DATES = [datetime.date(2019, 6, 16)]
 TALKS_ROOMS = [
-    (1, 'Main'),
-    (2, 'Theatre'),
+    (1, 'Ball room'),
+    (2, 'Club'),
 ]
 WORKSHOPS_ROOMS = [
-    (4, 'room 301'),
-    (5, 'room 302'),
-    (6, 'room 303'),
-    (7, 'room 343'),
-    (8, 'room 346'),
-    (9, 'room 347'),
-    (10, 'room 348'),
-    (11, 'room 349'),
-    (20, 'Respirium'),
+    (4, 'room EB126'),
+    (5, 'room EB129'),
+    (6, 'room EB130'),
+    (7, 'room EB226'),
+    (8, 'room EB229'),
+    (9, 'room EB230'),
 ]
 SPRINT_ROOMS = [
 
@@ -201,9 +198,8 @@ SPRINT_ROOMS = [
 
 OTHER_ROOMS = [
     (30, 'Foyer'),
-    (40, 'Backyard'),
-    (50, 'Red room'),
-    (60, 'Fish tank'),
+    (40, 'Compress hall'),
+    (50, 'Gallery'),
 ]
 
 ALL_ROOMS = TALKS_ROOMS + WORKSHOPS_ROOMS + SPRINT_ROOMS + OTHER_ROOMS
