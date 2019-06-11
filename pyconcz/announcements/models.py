e8c5d2b7b443a585549e2b0964287596f4d30abe
DanielSrb <srb.dan@gmail.com>
2019-06-11 08:07:42 +0200

    added font size option for annoucement + showing it admin list

--- pyconcz/announcements/models.py
+++ pyconcz/announcements/models.py
@@ -1,10 +1,19 @@
 from django.db import models
+from django.core.validators import MaxValueValidator, MinValueValidator
 
 
 class Announcement(models.Model):
     message = models.TextField(help_text='Markdown flavoured')
     position = models.PositiveSmallIntegerField(default=0)
     is_public = models.BooleanField(default=False)
+    font_size = models.PositiveSmallIntegerField(
+        default=1,
+        validators=[
+            MinValueValidator(1),
+            MaxValueValidator(4),
+        ],
+        help_text='1 (largest) to 4 (smallest)'
+    )
 
     class Meta:
         ordering = ['position']
