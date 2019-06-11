f995f440a10167c9b3bc7274b84ea6fb002a1efc
DanielSrb <srb.dan@gmail.com>
2019-06-11 15:30:11 +0200

    intermissions RC1

--- pyconcz/intermission/views.py
+++ pyconcz/intermission/views.py
@@ -1,58 +1,66 @@
 #!/usr/bin/env python
 # coding=utf-8
-import random
 import datetime
 
-from django.shortcuts import render
+from django.template.response import TemplateResponse
 
-from pyconcz.settings import BASE_DIR
 from pyconcz.sponsors.models import Sponsor
 from pyconcz.programme.models import Slot
 from pyconcz.announcements.models import Announcement
 
 
-gradients = [
-    'pc-gradient-orchid-ginger',
-    'pc-gradient-orchid-violetred',
-    'pc-gradient-orchid-yellow',
-    'pc-gradient-orchidred-yellow',
-    'pc-gradient-orchid-lime',
-    'pc-gradient-violetred-lime',
-    'pc-gradient-yellow-lime',
-    'pc-gradient-orchid-mint',
-    'pc-gradient-orchidred-cyan',
-    'pc-gradient-yellow-cyan',
-    'pc-gradient-orchid-blue',
-    'pc-gradient-yellow-blue',
-    'pc-gradient-orchidred-navy',
-    'pc-gradient-orchid-orchidred',
-    'pc-gradient-blue-cyan',
-    'pc-gradient-navy-cyan',
-]
-
-
 def index(request):
-    return render(request, 'intermission/index.html', context=dict(interval=int(request.GET.get('interval', 10)) * 1000))
+    return TemplateResponse(
+        request,
+        template='intermission/index.html',
+        context={
+            'interval': int(request.GET.get('interval', 10)) * 1000,
+        }
+    )
 
 
 def sponsors(request, level):
     _, val, display = Sponsor.LEVELS.for_constant(level)
     sponsors = Sponsor.objects.filter(published=True, level=val)
-    return render(request, 'intermission/sponsors.html',
-                  context=dict(sponsors=sponsors, level_name=display, level_slug=level))
+
+    return TemplateResponse(
+        request,
+        template='intermission/sponsors.html',
+        context={
+            'sponsors': sponsors,
+            'level_name': display,
+            'level_slug': level,
+        }
+    )
 
 
 def up_next(request, track):
     dt_from = datetime.datetime.now()
-    slot = Slot.objects.filter(date__gte=dt_from, room=track, object_id__isnull=False).order_by('date').first()
-    pc_gradient = random.choice(gradients) + random.choice(['', '-reversed'])
-    return render(request, 'intermission/up_next.html', dict(slot=slot, pc_gradient=pc_gradient))
+    slot = Slot.objects.filter(
+        start__gte=dt_from, room=track, object_id__isnull=False
+    ).order_by('start').first()
+
+    return TemplateResponse(
+        request,
+        template='intermission/up_next.html',
+        context={
+            'slot': slot,
+        }
+    )
 
 
 def announcements(request):
-    return render(request, 'intermission/annoucements.html',
-                  dict(announcements=Announcement.objects.filter(is_public=True)[:7]))
+    return TemplateResponse(
+        request,
+        template='intermission/annoucements.html',
+        context={
+            'announcements': Announcement.objects.filter(is_public=True)[:7],
+        }
+    )
 
 
 def slido(request):
-    return render(request, 'intermission/slido.html', {})
+    return TemplateResponse(
+        request,
+        template='intermission/slido.html',
+    )
