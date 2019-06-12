93207fdbc2c0556017d103daa0bb8cdc4ecf70cd
DanielSrb <srb.dan@gmail.com>
2019-06-12 09:29:34 +0200

    video frames RC1

--- pyconcz/programme/urls.py
+++ pyconcz/programme/urls.py
@@ -2,10 +2,11 @@ from django.conf.urls import url
 from django.views.generic import RedirectView
 
 from .views import talks_list, workshops_list, schedule, workshops_refresh_tickets
-from .views import session_detail, preview
+from .views import session_detail, talk_frame, preview
 
 urlpatterns = [
     url('^$', RedirectView.as_view(pattern_name='talks_list')),
+    url('^talk-frame/(?P<type>(video-only|16-9|4-3|intro))-(?P<session_id>\\d+)$', talk_frame, name='talk_frame'),
     url('^(?P<type>(talk|workshop|sprint))s/(?P<session_id>\\d+)/$', session_detail, name='session_detail'),
     url('^talks/$', talks_list, name='talks_list'),
     url('^workshops/$', workshops_list, name='workshops_list'),
