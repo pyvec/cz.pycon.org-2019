351d8f2e23064834bdc368680b5b3e89fb3f0a9d
DanielSrb <srb.dan@gmail.com>
2019-10-23 16:11:24 +0200

    new url for talk frames

--- pyconcz/programme/urls.py
+++ pyconcz/programme/urls.py
@@ -6,7 +6,7 @@ from .views import session_detail, talk_frame, preview
 
 urlpatterns = [
     url('^$', RedirectView.as_view(pattern_name='talks_list')),
-    url('^talk-frame/(?P<type>(video-only|16-9|4-3|intro))-(?P<session_id>\\d+)$', talk_frame, name='talk_frame'),
+    url('^talk-frame/(?P<frame_type>(video-only|16-9|4-3|intro))/(?P<session_id>\\d+)$', talk_frame, name='talk_frame'),
     url('^(?P<type>(talk|workshop|sprint))s/(?P<session_id>\\d+)/$', session_detail, name='session_detail'),
     url('^talks/$', talks_list, name='talks_list'),
     url('^workshops/$', workshops_list, name='workshops_list'),
