2f1c43cf3eb961d202945d58a5f9753c332e4c97
DanielSrb <srb.dan@gmail.com>
2020-01-14 11:11:50 +0100

    final video!

--- pyconcz/urls.py
+++ pyconcz/urls.py
@@ -26,7 +26,7 @@ prefixed_urlpatterns = [
     url(r'^pattern-lib/$', TemplateView.as_view(template_name='pages/pattern-lib.html'), name='pattern_lib'),
 
     # redirects to external websites
-    url(r'^video/$', RedirectView.as_view(url='https://www.youtube.com/watch?v=l_AOE0DbXiU')),
+    url(r'^video/$', RedirectView.as_view(url='https://www.youtube.com/watch?v=kL2gmhs0irI')),
 
 
     # static pages
